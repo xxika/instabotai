066066f898be32060331e61070ef0f32202e11bb
xxika <xiaoxi@xxika.cn>
2019-10-06 18:04:12 +0800

    Update run.py

--- run.py
+++ run.py
@@ -637,4 +637,4 @@ def multibot():
                        following=following, media_count=media_count);
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
